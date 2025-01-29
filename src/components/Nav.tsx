30bf006c6e179f87f91379df134a358e27d0a9b5
alfonso.lorenzana@gmail.com <alfonso.lorenzana@gmail.com>
2025-01-28 23:25:28 -0500

    New code to see CandidateSearch and Nav are working

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,24 @@
+import { Link, useLocation } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
-  return (
+  return (<>
     <div>Nav</div>
+    <Link
+          to="/"
+          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+        >
+          Home
+        </Link>
+        <Link
+          to="/ PotentialCandidates"
+          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+        >
+          PotentialCandidates
+        </Link>
+  </>
   )
 };
 
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,14 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 
+const username = "Question"
+const location = "UK, London"
+const email = "watermelon_2121212@gmail.com"
+const company = "American Software"
+const bio = "First time doing this"
+
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  return <><h1>{username}</h1> {location} {email} {company} {bio}</>;
 };
 
 export default CandidateSearch;
